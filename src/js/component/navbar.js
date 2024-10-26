429fa56a2552c93ac6502fa47a24f5f851fb3325
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-10-26 00:20:55 +0000

    Did styling on navbar and fixed alignment on dropdown to right.

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,27 +9,25 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 ps-2"><img width="48" height="48" src="https://img.icons8.com/color/48/star-wars.png" alt="star-wars"/></span>
 			</Link>
-			<div className="ml-auto">
-				
+			<div className="ml-auto pe-2">
 			<div className="dropdown">
 					<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 						Favorites
 					</a>
 
-					<ul className="dropdown-menu">
+					<ul className="dropdown-menu dropdown-menu-end dropdown-menu-start">
 						{
 							store.favs.length > 0 ? 
 							(store.favs.map(
 								(fav, index) => 
 									
 									{  
-									console.log(fav, "faveeee")
-									return (<li key={index}><a className="dropdown-item" href="#">{fav}</a></li>) 
+									return (<li key={index}><a className="dropdown-item ps-2" href="#">{fav}</a></li>) 
 									}
 							)
-						) : (<li>No Favorites </li>)  
+						) : (<li className="ps-2">No Favorites </li>)  
 						} 
 						
 					</ul>
