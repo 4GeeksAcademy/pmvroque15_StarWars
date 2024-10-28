4ffce6cf8cc2cdbda365f5a3c63b5ddc01d5a1d1
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-10-28 02:29:45 +0000

    deleted a component. finalized

--- src/js/views/starshipDescription.js
+++ src/js/views/starshipDescription.js
@@ -15,37 +15,42 @@ export default function StarshipDescription() {
   }, [id]);
 
   return (
-    <div className="d-flex justify-content-center mx-4 my-auto" >
-      <div className="card mb-5" style={{"background-color" : "transparent"}} >
-        <div className="row g-0" >
+    <div className="d-flex justify-content-center mx-4 my-auto">
+      <div className="card mb-5" style={{ "background-color": "transparent" }}>
+        <div className="row g-0">
           <div className="col-md-3">
             <img
               src={`https://starwars-visualguide.com/assets/img/starships/${info.uid}.jpg`}
               className=" object-fit-fill rounded"
             />
           </div>
-          <div className="col-md-8" >
-            <div className="card-body" style= {{ "float" : "right", "padding-left" : "250px" }} >
-              <h1 className="planetName card-title">{info.properties?.model}</h1>
+          <div className="col-md-8">
+            <div
+              className="card-body"
+              style={{ float: "right", "padding-left": "250px" }}
+            >
+              <h1 className="planetName card-title">
+                {info.properties?.model}
+              </h1>
               <p className="card-text ">
-                Starship Class: 
-               {info.properties?.starship_class}
+                Starship Class:
+                {info.properties?.starship_class}
               </p>
               <p className=" card-text">
-                Manufacturer: 
-              {info.properties?.manufacturer}
+                Manufacturer:
+                {info.properties?.manufacturer}
               </p>
               <p className=" card-text">
                 Length:
-              {info.properties?.length}
+                {info.properties?.length}
               </p>
               <p className=" card-text">
                 Crew:
-              {info.properties?.crew}
+                {info.properties?.crew}
               </p>
               <p className=" card-text">
                 Passengers:
-              {info.properties?.passengers}
+                {info.properties?.passengers}
               </p>
             </div>
           </div>
